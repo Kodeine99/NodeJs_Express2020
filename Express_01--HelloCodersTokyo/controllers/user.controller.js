ab21af638fbad78efc5e3725ef8fc7907e4ba603
Kodeine <cubinngo1102@gmail.com>
2020-06-04 09:51:26 +0700

    Create controllers directory which contains the logical processing of each function in routers

---
+++ Express_01--HelloCodersTokyo/controllers/user.controller.js
@@ -0,0 +1,42 @@
+var db = require('../db');
+const shortid = require('shortid');
+
+
+// Đưa logic xử lí của hàm vào trong file controller theo mô hình MVC (design pattern)
+module.exports.index = function (req, res) {
+    res.render('Users/index', {
+        Users: db.get('Users').value()
+    })
+};
+
+module.exports.search = function (req, res) {
+    let q = req.query.q;
+    let matchedUsers = db.get('Users').value().filter(function (user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+    res.render('Users/index', {
+        Users: matchedUsers
+    });
+};
+
+module.exports.create = function (req, res) {
+    res.render('Users/Create');
+};
+
+module.exports.get = function (req, res) {
+
+    let id = req.params.id;
+    let user = db.get('Users').find({ id: id }).value();
+
+    res.render('Users/view', {
+        user: user
+    });
+};
+
+module.exports.postCreate = function (req, res) {
+    req.body.id = shortid.generate();
+    db.get('Users').push(req.body).write();
+    res.redirect('/Users');
+    // console.log(req.body);
+    // res.json(req.body)
+};
