997f4053d0afa870c28aa0b2e5eb361c585e9a24
Kodeine <cubinngo1102@gmail.com>
2020-06-12 16:40:15 +0700

    Add function to add avatar for users when creating new user

--- Express_01--HelloCodersTokyo/routes/user.route.js
+++ Express_01--HelloCodersTokyo/routes/user.route.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var multer = require('multer');
+
+var upload = multer({ dest: './public/uploads' });
 
 var router = express.Router();
 var controller = require('../controllers/user.controller');
@@ -18,7 +21,7 @@ router.get('/Create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/Create', validate.postCreate, controller.postCreate);
+router.post('/Create',upload.single('avatar'), validate.postCreate, controller.postCreate);
 
 module.exports = router;
 
